fdc2db2a2d3044d62ff8afb8cb3ff07b9317c7e5
Yun <duwls0349@naver.com>
2022-11-07 23:13:00 +0900

    [Feat] UIColor Extension 생성

---
+++ SeSAC_StudyMatchingApp/Utility/Extension/UIColor+Extension.swift
@@ -0,0 +1,51 @@
+//
+//  UIColor+Extension.swift
+//  SeSAC_StudyMatchingApp
+//
+//  Created by 윤여진 on 2022/11/07.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var green: UIColor {
+        UIColor(red: 0.285, green: 0.863, blue: 0.574, alpha: 1.0)
+    }
+    static var whiteGreen: UIColor {
+        UIColor(red: 0.804, green: 0.958, blue: 0.881, alpha: 1.0)
+    }
+    static var yellowGreen: UIColor {
+        UIColor(red: 0.7, green: 0.921, blue: 0.381, alpha: 1.0)
+    }
+    static var gray1: UIColor {
+        UIColor(red: 0.969, green: 0.969, blue: 0.969, alpha: 1)
+    }
+    static var gray2: UIColor {
+        UIColor(red: 0.937, green: 0.937, blue: 0.937, alpha: 1)
+    }
+    static var gray3: UIColor {
+        UIColor(red: 0.887, green: 0.887, blue: 0.887, alpha: 1)
+    }
+    static var gray4: UIColor {
+        UIColor(red: 0.821, green: 0.821, blue: 0.821, alpha: 1)
+    }
+    static var gray5: UIColor {
+        UIColor(red: 0.742, green: 0.742, blue: 0.742, alpha: 1)
+    }
+    static var gray6: UIColor {
+        UIColor(red: 0.667, green: 0.667, blue: 0.667, alpha: 1)
+    }
+    static var gray7: UIColor {
+        UIColor(red: 0.533, green: 0.533, blue: 0.533, alpha: 1)
+    }
+    static var success: UIColor {
+        UIColor(red: 0.384, green: 0.559, blue: 0.9, alpha: 1)
+    }
+    static var error: UIColor {
+        UIColor(red: 0.913, green: 0.401, blue: 0.419, alpha: 1)
+    }
+    static var focus: UIColor {
+        UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    }
+}
