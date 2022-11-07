685973ef69b56131ef06c27d0557c0eb3bccd5d8
Yun <duwls0349@naver.com>
2022-11-07 23:12:07 +0900

    [Feat] BaseCollectionViewCell & BaseTableViewCell 생성

---
+++ SeSAC_StudyMatchingApp/Utility/BaseView/BaseCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BaseCollectionViewCell.swift
+//  SeSAC_StudyMatchingApp
+//
+//  Created by 윤여진 on 2022/11/07.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        setConstraints()
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() { }
+    
+    func setConstraints() { }
+    
+}
+
---
+++ SeSAC_StudyMatchingApp/Utility/BaseView/BaseTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  BaseTableViewCell.swift
+//  SeSAC_StudyMatchingApp
+//
+//  Created by 윤여진 on 2022/11/07.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureUI()
+        setConstraints()
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() { }
+    
+    func setConstraints() { }
+    
+}
