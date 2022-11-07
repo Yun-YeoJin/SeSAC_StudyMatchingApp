1e18be2a551250542b570a96c2b6d6f6a9c44034
Yun <duwls0349@naver.com>
2022-11-07 13:22:20 +0900

    Initial Commit

---
+++ SeSAC_StudyMatchingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeSAC_StudyMatchingApp
+//
+//  Created by 윤여진 on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
