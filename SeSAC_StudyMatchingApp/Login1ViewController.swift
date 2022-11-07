9bfa500a263b756cb6ed00db0dc46b743dd7f837
Yun <duwls0349@naver.com>
2022-11-07 23:48:35 +0900

    [Feat] Login1 - 전화번호 입력 시 010-1234-5678 형식으로 format 설정

--- SeSAC_StudyMatchingApp/Login1ViewController.swift
+++ SeSAC_StudyMatchingApp/Login1ViewController.swift
@@ -23,15 +23,48 @@ final class Login1ViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         mainView.getAuthButton.rx.tap
             .withUnretained(self)
             .subscribe { vc, _ in
                 vc.navigationController?.pushViewController(Login2ViewController(), animated: true)
             }.disposed(by: disposeBag)
         
+        bind()
     }
     
+    func bind() {
+        
+        mainView.phoneNumber.rx.text.orEmpty
+            .changed
+            .bind { value in
+                self.mainView.phoneNumber.text = self.phoneNumberformat(with: "XXX-XXXX-XXXX", phone: value)
+            }
+            .disposed(by: disposeBag)
+        
+    }
+    
+    func phoneNumberformat(with mask: String, phone: String) -> String {
+        
+        let numbers = phone.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
+        var result = ""
+        var index = numbers.startIndex // numbers iterator
+
+        // iterate over the mask characters until the iterator of numbers ends
+        for ch in mask where index < numbers.endIndex {
+            if ch == "X" {
+                // mask requires a number in this place, so take the next one
+                result.append(numbers[index])
+
+                // move numbers iterator to the next index
+                index = numbers.index(after: index)
+
+            } else {
+                result.append(ch) // just append a mask character
+            }
+        }
+        return result
+        
+    }
    
     
 }
