73511d5770c38e62347285d0122f0b7d5e1aaedf
Yun <duwls0349@naver.com>
2022-11-10 21:02:28 +0900

    [Feat] UserDefaultKey 생성

---
+++ SeSAC_StudyMatchingApp/Utility/Constants/UserDefaultKey.swift
@@ -0,0 +1,23 @@
+//
+//  UserDefaultKey.swift
+//  SeSAC_StudyMatchingApp
+//
+//  Created by 윤여진 on 2022/11/09.
+//
+
+import Foundation
+
+enum UserDefaultKey {
+    
+    static let secondRun = "secondRun"
+    static let authVerificationID = "authVerificationID"
+    static let credentialId = "credentialId"
+    static let fcmToken = "FCMToken"
+    static let idToken = "idToken"
+    static let phoneNumber = "phoneNumber"
+    static let nickname = "nickname"
+    static let birth = "birth"
+    static let email = "email"
+    static let gender = "gender"
+    
+}
