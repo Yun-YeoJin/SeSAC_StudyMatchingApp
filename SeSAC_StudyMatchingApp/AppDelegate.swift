5c211fe9edbdf21097683eaf249f595bc4a802f0
Yun <duwls0349@naver.com>
2022-11-11 20:30:00 +0900

    [Feat] FirebaseMessaging 연결 -> FCM 토큰 받기

--- SeSAC_StudyMatchingApp/AppDelegate.swift
+++ SeSAC_StudyMatchingApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
+        
+        //메시지 대리자 설정
+        Messaging.messaging().delegate = self
+        
+        //현재 등록된 토큰 가져오기
+        Messaging.messaging().token { token, error in
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+            }
+        }
         
         sleep(1)
         
@@ -37,5 +50,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    //토큰 갱신 모니터링
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    
 }
 
