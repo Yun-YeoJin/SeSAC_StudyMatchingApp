968ae1088691f739f67b84fe4c5e441969a09c70
Yun <duwls0349@naver.com>
2022-11-17 02:40:16 +0900

    [Feat] UIViewController+Extension : 키보드 숨기기, Alert 띄우기 추가

--- SeSAC_StudyMatchingApp/Utility/Extension/UIViewController+Extension.swift
+++ SeSAC_StudyMatchingApp/Utility/Extension/UIViewController+Extension.swift
@@ -30,4 +30,25 @@ extension UIViewController {
         UserDefaultsRepository.saveBirth(birth: newDate)
     }
     
+    public func showAlert(title: String, message: String, buttonTitle: String, buttonAction: @escaping ((UIAlertAction) -> Void )) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: buttonTitle, style: .destructive, handler: buttonAction)
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        self.present(alert, animated: true)
+        
+    }
+    
+    public func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
 }
