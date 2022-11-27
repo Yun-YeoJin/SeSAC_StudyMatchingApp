417be5aaf91fbcfcec2652f0b78b660752fd516a
Yun <duwls0349@naver.com>
2022-11-27 23:35:32 +0900

    [Feat] Queue Endpoint 생성

--- SeSAC_StudyMatchingApp/Network/EndPoint.swift
+++ SeSAC_StudyMatchingApp/Network/EndPoint.swift
@@ -15,6 +15,10 @@ enum Endpoint {
     case requestSearchUser, cancelSearchUser
     case searchSeSAC
     case checkUserQueueState
+    case studyRequest
+    case studyAccept
+    case dodge
+    case review
 }
 
 extension Endpoint {
@@ -30,11 +34,19 @@ extension Endpoint {
         case .userMyPage:
             return .makeEndpoint(("/v1/user/mypage"))
         case .requestSearchUser, .cancelSearchUser:
-            return .makeEndpoint("/v1/queue")
+            return .makeEndpoint("/v1/queue") //새싹 찾기 요청 및 중단
         case .searchSeSAC:
-            return .makeEndpoint("/v1/queue/search")
+            return .makeEndpoint("/v1/queue/search") //주변 새싹 탐색 기능
         case .checkUserQueueState:
-            return .makeEndpoint("/v1/queue/myQueueState")
+            return .makeEndpoint("/v1/queue/myQueueState") //사용자(본인)의 매칭상태 확인
+        case .studyRequest:
+            return .makeEndpoint("/v1/queue/studyrequest") //스터디 요청하기
+        case .studyAccept:
+            return .makeEndpoint("/v1/queue/studyaccept") //스터디 수락하기
+        case .dodge:
+            return .makeEndpoint("/v1/queue/dodge") //스터디 취소하기
+        case .review:
+            return .makeEndpoint("/v1/queue/rate/") //리뷰 등록하기
         }
     }
     
