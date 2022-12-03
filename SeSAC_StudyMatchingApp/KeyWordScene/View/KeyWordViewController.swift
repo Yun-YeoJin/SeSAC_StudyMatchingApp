1b1a8df4a9974ee70bedb9ba4bcfc5c6b5d2afa6
Yun <duwls0349@naver.com>
2022-12-03 23:12:09 +0900

    [Feat] 키워드 검색 로직 구현

--- SeSAC_StudyMatchingApp/KeyWordScene/View/KeyWordViewController.swift
+++ SeSAC_StudyMatchingApp/KeyWordScene/View/KeyWordViewController.swift
@@ -7,14 +7,25 @@
 
 import UIKit
 
+import SnapKit
 import RxCocoa
 import RxSwift
+import RxKeyboard
 
 final class KeyWordViewController: BaseViewController {
     
+    let searchBar = UISearchBar().then {
+        $0.placeholder = "띄어쓰기로 복수 입력이 가능해요"
+        $0.autocorrectionType = .no
+        $0.autocapitalizationType = .none
+    }
+    
     let mainView = KeyWordView()
+    let viewModel = KeyWordViewModel()
     
     var disposeBag = DisposeBag()
+    
+    let maxLength = 8
     
     override func loadView() {
         self.view = mainView
@@ -23,14 +34,23 @@ final class KeyWordViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("myIdToken : \(UserDefaultsRepository.fetchUserIDToken())")
+        
+        searchNearSeSAC()
+        
         mainView.collectionView.delegate = self
         mainView.collectionView.dataSource = self
         
+        bindSearchBar()
+        bindRxKeyboard()
+        setGesture()
+        bindButton()
+        
     }
-    
+
     override func configureUI() {
        
-        self.navigationItem.titleView = mainView.searchBar
+        self.navigationItem.titleView = searchBar
         self.navigationItem.leftBarButtonItem = mainView.backBarButton
         mainView.backBarButton.target = self
         mainView.backBarButton.rx.tap
@@ -38,6 +58,131 @@ final class KeyWordViewController: BaseViewController {
                 self?.navigationController?.popViewController(animated: true)
             }.disposed(by: disposeBag)
         
+    }
+    
+    func searchNearSeSAC() {
+        
+        print("Lat은 몇이게요 ?? \(viewModel.lat.value)")
+        print("Long은 몇이게요 ?? \(viewModel.long.value)")
+        viewModel.searchNearSeSAC { [self] message, code in
+            switch code {
+            case .userUnexist:
+                self.view.makeToast("사용자가 없습니다.")
+            default:
+                self.view.makeToast("\(code)")
+            }
+        }
+    }
+    
+    private func bindButton() {
+        mainView.findSeSACButton.rx.tap
+            .withUnretained(self)
+            .subscribe(onNext: { vc, _ in
+                vc.viewModel.searchQueue { message, code in
+                    switch code {
+                    case .success:
+                        vc.transition(FindNearUserViewController(), transitionStyle: .push)
+                    default:
+                        vc.view.makeToast("\(code.rawValue) 오류 발생", position: .center)
+                        print(vc.viewModel.long.value)
+                        print(vc.viewModel.lat.value)
+                        print(vc.viewModel.studyListUser)
+                    }
+                }
+            }).disposed(by: disposeBag)
+    }
+    
+    private func bindSearchBar() {
+        
+        // 텍스트 변경시
+        searchBar.rx.text.orEmpty
+            .debounce(RxTimeInterval.microseconds(5), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .subscribe(onNext: { [self] text in
+                if text.count > maxLength { // 최대 8자
+                    self.view.makeToast("취미는 최대 8자만 가능해요.")
+                    let index = text.index(text.startIndex, offsetBy: maxLength)
+                    let newString = text[text.startIndex..<index]
+                    searchBar.text = String(newString)
+                }
+            }).disposed(by: disposeBag)
+        
+        // search 버튼 눌렸을 경우
+        searchBar.rx.searchButtonClicked
+            .withUnretained(self)
+            .subscribe(onNext: { vc, data in
+                let StudyList = vc.viewModel.getStudyData(self.searchBar.text!)
+                switch StudyList {
+                case .full:
+                    vc.view.makeToast("8개까지 등록 가능합니다.")
+                    return
+                case .empty:
+                    return
+                case .success:
+                    vc.mainView.collectionView.reloadData()
+                case .invalid:
+                    vc.view.makeToast("유효하지 않은 값입니다.")
+                    return
+                case .contained:
+                    vc.view.makeToast("중복된 값입니다.")
+                    return
+                }
+                vc.searchBar.text = ""
+            }).disposed(by: disposeBag)
+    }
+    
+    private func setGesture() {
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(_:)))
+        swipeDown.direction = UISwipeGestureRecognizer.Direction.down
+        self.mainView.addGestureRecognizer(swipeDown)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(_:)))
+        swipeUp.direction = UISwipeGestureRecognizer.Direction.up
+        self.mainView.addGestureRecognizer(swipeUp)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismisskeyboard))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismisskeyboard() {
+        searchBar.endEditing(true)
+    }
+    
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer{
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.up:
+                searchBar.becomeFirstResponder()
+            case UISwipeGestureRecognizer.Direction.down:
+                searchBar.endEditing(true)
+            default:
+                break
+            }
+        }
+    }
+    
+    private func bindRxKeyboard() {
+        RxKeyboard.instance.visibleHeight
+            .drive { [self] height in
+                if height == 0 {
+                    mainView.findSeSACButton.layer.cornerRadius = 8
+                    mainView.findSeSACButton.snp.updateConstraints { make in
+                        make.leading.trailing.equalToSuperview().inset(10)
+                        make.height.equalTo(48)
+                        make.bottom.equalTo(mainView.safeAreaLayoutGuide)
+                    }
+                } else {
+                    mainView.findSeSACButton.layer.cornerRadius = 0
+                    mainView.findSeSACButton.snp.updateConstraints { make in
+                        make.bottom.equalTo(mainView.safeAreaLayoutGuide).inset(height - mainView.safeAreaInsets.bottom)
+                        make.leading.trailing.equalToSuperview()
+                    }
+                }
+                // 애니메이션
+                view.layoutIfNeeded()
+            }
+            .disposed(by: disposeBag)
     }
     
 }
@@ -49,11 +194,70 @@ extension KeyWordViewController: UICollectionViewDelegate, UICollectionViewDataS
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        switch section {
+        case 0:
+            return viewModel.fromRecommend.value.count + viewModel.studyListData.count
+        case 1:
+            return viewModel.studyListUser.count
+        default:
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KeyWordCollectionViewCell.reuseIdentifier, for: indexPath) as? KeyWordCollectionViewCell else { return UICollectionViewCell() }
+        
+        let StudyListDataIndex = viewModel.fromRecommend.value.count
+        let recommendIndex = self.viewModel.fromRecommend.value.count
+        
+        switch indexPath.section {
+        case 0:
+            if indexPath.row >= viewModel.fromRecommend.value.count {
+                cell.shellView.layer.borderColor = UIColor.gray6.cgColor
+                cell.label.textColor = .label
+                cell.label.text = viewModel.studyListData[StudyListDataIndex]
+                cell.button.isHidden = true
+                cell.cellTapActionHandler = {
+                    if self.viewModel.studyListUser.count >= 8 {
+                        self.view.makeToast("8개까지 등록 가능합니다.")
+                        return
+                    }
+                    if !self.viewModel.studyListUser.contains(self.viewModel.studyListData[indexPath.row - self.viewModel.fromRecommend.value.count]) {
+                        self.viewModel.studyListUser.append(self.viewModel.studyListData[indexPath.row - recommendIndex])
+                        self.mainView.collectionView.reloadData()
+                    }
+                    
+                }
+            } else {
+                cell.shellView.layer.borderColor = UIColor.red.cgColor
+                cell.label.textColor = .red
+                cell.label.text = viewModel.fromRecommend.value[indexPath.row]
+                cell.button.isHidden = true
+                cell.cellTapActionHandler = {
+                    if self.viewModel.studyListUser.count >= 8 {
+                        self.view.makeToast("8개까지 등록 가능합니다.")
+                        return
+                    }
+                    if !self.viewModel.studyListUser.contains(self.viewModel.fromRecommend.value[indexPath.row]) {
+                        self.viewModel.studyListUser.append(self.viewModel.fromRecommend.value[indexPath.row])
+                        self.mainView.collectionView.reloadData()
+                    }
+                }
+            }
+        case 1:
+            cell.shellView.layer.borderColor = UIColor.green.cgColor
+            cell.label.textColor = .green
+            cell.label.text = viewModel.studyListUser[indexPath.row]
+            cell.button.isHidden = false
+            cell.cellTapActionHandler = {
+                // 셀 삭제
+                self.viewModel.studyListUser.remove(at: indexPath.row)
+                self.mainView.collectionView.reloadData()
+            }
+        default:
+            print(#function, "default")
+        }
+
         
         return cell
     }
@@ -82,8 +286,6 @@ extension KeyWordViewController: UICollectionViewDelegate, UICollectionViewDataS
             return CGSize(width: collectionView.frame.width, height: 40)
         case 1:
             return CGSize(width: collectionView.frame.width, height: 40)
-        case 2:
-            return CGSize(width: collectionView.frame.width, height: 40)
         default:
             return CGSize(width: collectionView.frame.width, height: 0)
         }
@@ -102,8 +304,6 @@ extension KeyWordViewController: UICollectionViewDelegateFlowLayout {
            $0.sizeToFit()
        }
        let size = label.frame.size
-       return CGSize(width: size.width + 34, height: size.height + 14)
+       return CGSize(width: size.width + 10, height: size.height + 14)
    }
 }
-
-
--- SeSAC_StudyMatchingApp/KeyWordScene/ViewModel/KeyWordViewModel.swift
+++ SeSAC_StudyMatchingApp/KeyWordScene/ViewModel/KeyWordViewModel.swift
@@ -6,3 +6,179 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+
+final class KeyWordViewModel {
+    
+    let lat = BehaviorRelay<Double>(value: 37.517819364682694)
+    let long = BehaviorRelay<Double>(value: 126.88647317074734)
+    let region = BehaviorRelay<Int>(value: 0)
+    
+    let data = BehaviorRelay<Queue>(value: Queue(fromQueueDB: [], fromQueueDBRequested: [], fromRecommend: []))
+    
+    let title = BehaviorRelay<[String]>(value: ["좋은 매너", "정확한 시간 약속", "빠른 응답", "친절한 성격", "능숙한 취미 실력", "유익한 시간"])
+    
+    var reputation = BehaviorRelay<[[Int]]>(value: [])
+    var fromRecommend: CObservable<[String]> = CObservable([])
+    
+    lazy var studyListData: [String] = []
+    lazy var studyListUser: [String] = []
+    
+    func caculateRegion() {
+        
+        let latTemp = String(lat.value + 90).components(separatedBy: ["."]).joined()
+        let longTemp = String(long.value + 180).components(separatedBy: ["."]).joined()
+        
+        let latEndIdx: String.Index = latTemp.index(latTemp.startIndex, offsetBy: 4)
+        let longEndIdx: String.Index = longTemp.index(longTemp.startIndex, offsetBy: 4)
+        
+        let latResult = String(latTemp[...latEndIdx])
+        let longResult = String(longTemp[...longEndIdx])
+    
+        region.accept(Int([latResult, longResult].joined())!)
+        
+    }
+    
+    func nearFriendsData(data: [FromQueueDB]) -> [String] {
+        var arr: [String] = []
+        
+        data.forEach {
+            $0.studylist.forEach {
+                arr.append($0.matchString(_string: $0))
+            }
+        }
+        
+        let removedDuplicate: Set = Set(arr)
+        arr = Array(removedDuplicate)
+        
+        return arr
+    }
+    
+    func getStudyData(_ data: String) -> StudyList {
+        var arr = data.components(separatedBy: " ")
+        arr = arr.filter { $0 != "" }
+        
+        if studyListUser.count > 8 {
+            return .full
+        }
+        
+        var tempArr: [String] = []
+        
+        tempArr += arr
+        tempArr += studyListUser
+        
+        if tempArr.isEmpty {
+            return .empty
+        }
+        
+        let removedDuplicate: Set = Set(tempArr)
+        tempArr = Array(removedDuplicate)
+        
+        print(tempArr)
+        
+        if tempArr.count > 8 {
+            return .full
+        }
+        
+        studyListUser = tempArr
+        
+        return .success
+    }
+    
+    func searchQueue(completion: @escaping (String, SearchQueueEnum) -> Void) {
+        APIService.shared.requestSearchUser(long: long.value, lat: lat.value, studylist: studyListUser, completion: { data, code in
+            switch code {
+            case .bannedUser:
+                completion("신고가 누적되어 이용하실 수 없습니다.", code)
+            case .studyCancelPenalty1:
+                completion("약속 취소 패널티로, 1분동안 이용하실 수 없습니다", code)
+            case .firebaseTokenInvalid:
+                self.getFCMToken { message, statusCode in
+                    switch statusCode {
+                    case .success:
+                        completion("", code)
+                    default:
+                        completion(message, code)
+                    }
+                }
+            case .studyCancelPenalty2:
+                completion("약속 취소 패널티로, 2분동안 이용하실 수 없습니다", code)
+            case .studyCancelPenalty3:
+                completion("연속으로 약속을 취소하셔서 3분동안 이용하실 수 없습니다", code)
+            default:
+                completion("", code)
+            }
+        })
+    }
+    
+    func searchNearSeSAC(completion: @escaping (String, SearchQueueEnum) -> Void) {
+        APIService.shared.searchNearSeSAC(long: long.value, lat: lat.value, completion: { [self] dataValue, code in
+            switch code {
+            case .success:
+                guard let queueData = dataValue else { return }
+                data.accept(queueData)
+                
+                print("data : ", data.value)
+                
+                var arr: [[Int]] = []
+                queueData.fromQueueDB.forEach {
+                    arr.append($0.reputation)
+                }
+                reputation.accept(arr)
+                
+                studyListData.removeAll()
+                studyListData += nearFriendsData(data: queueData.fromQueueDB)
+                studyListData += nearFriendsData(data: queueData.fromQueueDBRequested)
+                
+                fromRecommend.value = queueData.fromRecommend
+            
+                completion("", code)
+            case .firebaseTokenInvalid:
+                self.getFCMToken { message, statusCode in
+                    switch statusCode {
+                    case .success:
+                        completion("", code)
+                    default:
+                        completion(message, code)
+                    }
+                }
+            default:
+                completion("", code)
+            }
+        })
+    }
+    
+    func deleteQueue(completion: @escaping (String, SearchQueueEnum) -> Void) {
+        APIService.shared.deleteOnqueue { [self] data, code in
+            switch code {
+            case .userUnexist:
+                print("이미 매칭된 상태")
+            case .firebaseTokenInvalid:
+                self.getFCMToken { message, statusCode in
+                    switch statusCode {
+                    case .success:
+                        completion("", code)
+                    default:
+                        completion(message, code)
+                    }
+                }
+            case .serverError:
+                print("server error")
+            default:
+                completion("", code)
+            }
+        }
+    }
+    
+    func getFCMToken(completion: @escaping (String, UserEnum) -> Void) {
+        FirebaseRepository.shared.updateFCMToken { status in
+            switch status {
+            case .serverError:
+                completion("에러가 발생했습니다. 잠시 후 다시 시도해주세요.", status)
+            default:
+                completion("", status)
+            }
+        }
+    }
+}
