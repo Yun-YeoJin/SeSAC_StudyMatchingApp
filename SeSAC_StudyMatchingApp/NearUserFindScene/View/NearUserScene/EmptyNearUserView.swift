aab5cf33b15d41a7d4b5b9d56359ce515986c06c
Yun <duwls0349@naver.com>
2022-12-04 17:16:22 +0900

    [Feat] 주변 새싹 / 받은 요청 EmptyView 생성

---
+++ SeSAC_StudyMatchingApp/NearUserFindScene/View/NearUserScene/EmptyNearUserView.swift
@@ -0,0 +1,78 @@
+//
+//  EmptyNearUserView.swift
+//  SeSAC_StudyMatchingApp
+//
+//  Created by 윤여진 on 2022/12/04.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class EmptyNearUserView: BaseView {
+    
+    let imageView = UIImageView().then {
+        $0.image = UIImage(named: "empty_img")
+        $0.backgroundColor = .clear
+    }
+    
+    let changeStudyButton = CustomButton().then {
+        $0.setTitle("스터디 변경하기", for: .normal)
+        $0.buttonState = .fill
+        $0.titleLabel?.font = .title4_R14
+    }
+    
+    let refreshButton = CustomButton().then {
+        $0.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
+        $0.buttonState = .outline
+    }
+    
+    let ButtonView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+       
+    }
+    
+    override func configureUI() {
+   
+        [imageView, ButtonView].forEach {
+            self.addSubview($0)
+        }
+        
+        [changeStudyButton, refreshButton].forEach {
+            ButtonView.addSubview($0)
+        }
+        
+    
+    }
+    
+    override func setConstraints() {
+        
+        imageView.snp.makeConstraints { make in
+            make.centerY.centerX.equalToSuperview()
+            make.width.equalTo(220)
+            make.height.equalTo(170)
+        }
+        
+        ButtonView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalTo(safeAreaLayoutGuide).offset(-16)
+            make.height.equalTo(48)
+        }
+        
+        changeStudyButton.snp.makeConstraints { make in
+            make.width.equalTo(287)
+            make.leading.equalToSuperview().inset(10)
+            make.bottom.top.equalToSuperview()
+        }
+        
+        refreshButton.snp.makeConstraints { make in
+            make.size.equalTo(48)
+            make.top.equalTo(changeStudyButton.snp.top)
+            make.leading.equalTo(changeStudyButton.snp.trailing).offset(10)
+        }
+    }
+}
+
